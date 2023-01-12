036614eed8244a72b8bef4568b8d439e842a897f
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2023-01-12 17:57:28 -0500

    final flag

--- Guess the Flag/ContentView.swift
+++ Guess the Flag/ContentView.swift
@@ -18,8 +18,8 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             RadialGradient(stops: [
-                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
-                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.4),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.4)
             ], center: .top, startRadius: 200, endRadius: 700)
                 .ignoresSafeArea()
 
@@ -53,7 +53,7 @@ struct ContentView: View {
                 }
                 .frame(maxWidth: .infinity)
                 .padding(.vertical, 20)
-                .background(.regularMaterial)
+                .background(.ultraThinMaterial)
                 .clipShape(RoundedRectangle(cornerRadius: 20))
 
                 Spacer()
